c720f867a75ead3c612737280922a262e21a3451
Russell Folk <rfolk@russellfolk.com>
2013-06-26 19:43:56 -0600

    Changed the upper bound to n*ln(n)*ln(n).
    Research shows that while this is not optimal it is acceptable and much better
    than n^2.

--- hw1/primes.js
+++ hw1/primes.js
@@ -1,29 +1,34 @@
 #!/usr/bin/env node
 /**
-	*	Will calculate the prime numebers from 1 to n
-	*/
+  * Will calculate the first "n" prime numbers
+  * Uses the Sieve of Eratosthenes to find them
+  * To find the "top value possible" I researched and it seems that the ceiling
+  * of n * ln( n ) * ln( n ) will give you a reasonable stopping point. It is
+  * not the tightest ceiling but it is far better than n^2.
+  */
 
 var fs    = require( 'fs' ) ;
 var file  = "primes.txt" ;
 var n     = 100 ;
-var top   = n * n ;
+var top   = Math.ceil( n * Math.log( n ) * Math.log( n ) ) ;
+//  Research shows that this top should provide a maximum for all primes
 
 var i , primes = [] , found = 0 , num = 2 ;
 
 var i , j , num = [] ;
 for ( i = 0 ; i < top ; i ++ )
-	num[ i ] = true ;
+  num[ i ] = true ;
 for ( i = 2 ; found < n ; i ++ )
 {
-	if ( num[ i ] == true )
-	{
-		fs.writeFileSync( file , i , { 'flag' : 'a' } ) ;
-		found ++ ;
-		if ( found < n )
-			fs.writeFileSync( file , "," , { 'flag' : 'a' } ) ;
-		for ( j = i * i ; j < top ; j += i )
-			num[ j ] = false ;
-	}
+  if ( num[ i ] == true )
+  {
+    fs.writeFileSync( file , i , { 'flag' : 'a' } ) ;
+    found ++ ;
+    if ( found < n )
+      fs.writeFileSync( file , "," , { 'flag' : 'a' } ) ;
+    for ( j = i * i ; j < top ; j += i )
+      num[ j ] = false ;
+  }
 }
 fs.writeFileSync( file , "\n" , { 'flag' : 'a' } ) ;
 
